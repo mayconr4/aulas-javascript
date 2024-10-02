373a04d9d0b40dc8b681e5505cb0a73fb8698e7f
mayconr4 <mayconhs2007@gmail.com>
2024-10-02 11:37:10 -0300

    exercicio de js tendo obj dentro de obj

--- js/04-objetos.js
+++ js/04-objetos.js
@@ -24,5 +24,57 @@ const livro = {
     ]
 } 
 
-// console.log(livro);
+// exibindo  a estrutura
+console.log(livro); 
+
+// Acesso usando nome da propriedade. e indice da array desta propriedade.
+console.log(livro.volumes[1]);
+
+// exemplo 3: array e objetos
+const livros = [
+    {
+        titulo: "Harry potter",
+        autor: "J.K rowling"
+    },  //esta vitgula separa os objetos sempre use no final desta chaves de exemplo 
+    {
+        titulo: "Senhor doa aneis",
+        autor: "J.K.K Toliken"
+    },  
+    {
+        titulo: "Ghost rhider", 
+        autor: "Neil Peart"
+    }
+
+]; 
+
+// Acessando através do indice a propridade.
+console.log(livros[1].autor); 
+
+// exercicio 
+
+
+
+
+const aluno = {
+   Nome:"Maycon Henrique Silva Santos",
+   data: "12/05/2007" ,
+   telefone: ["11-98020-4731", "11-97504-0149"] , 
+
+   endereco: {
+    rua:"Rua Guilherme de oliveira sá",
+    numero: "625" ,
+    bairro: "ermelino matarazzo"   
+   }
+} 
+
+
+
+console.log(aluno.Nome);
+console.log(aluno.telefone[1]); 
+console.log(aluno.endereco.bairro); 
+
+console.log(`o aluno ${aluno.Nome}, cujo telefone celular é ${aluno.telefone[1]}, que mora no bairro ${aluno.endereco.bairro}.`);
+
+
+
 
